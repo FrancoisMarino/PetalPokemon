9c976f2f42dd1df6b1fd097d8b5ee33e892c2efc
FrancoisMarino <francois.marino5@gmail.com>
2021-11-13 11:22:25 -0500

    add pokemons scaffold

---
+++ petalpokemon_api/db/migrate/20211113162156_create_pokemons.rb
@@ -0,0 +1,17 @@
+class CreatePokemons < ActiveRecord::Migration[6.1]
+  def change
+    create_table :pokemons do |t|
+      t.string :name
+      t.integer :HP
+      t.integer :attack
+      t.integer :defense
+      t.integer :speedAttack
+      t.integer :speedDefense
+      t.integer :speed
+      t.integer :generation
+      t.boolean :lengendary
+
+      t.timestamps
+    end
+  end
+end
